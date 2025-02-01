23db740ff385c125d728fbf5f37b38e105a03ee6
tcaothien <116654835+tcaothien@users.noreply.github.com>
2025-02-01 22:30:23 +0700

    Update welcomecards.js

--- UI/banners/welcomecards.js
+++ UI/banners/welcomecards.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 const data = {
     welcomeImages: [
-        path.resolve(__dirname, '../welcomeimages/1.png'),
+        path.resolve(__dirname, '../welcomeimages/1.jpg'),
         path.resolve(__dirname, '../welcomeimages/2.jpg'),
         path.resolve(__dirname, '../welcomeimages/3.jpg'),
     ],
