82d0c2dfc09d7f37a4e7cd6575aa9f2764db9b7e
tcaothien <116654835+tcaothien@users.noreply.github.com>
2025-02-01 21:18:40 +0700

    Update roleCreateHandler.js

--- logHandlers/roleCreateHandler.js
+++ logHandlers/roleCreateHandler.js
@@ -1,18 +1,20 @@
 const { logsCollection } = require('../mongodb');
 const { EmbedBuilder } = require('discord.js');
+
 module.exports = async function roleCreateHandler(client) {
     client.on('roleCreate', async (role) => {
+        // Lấy cấu hình ghi log cho sự kiện tạo vai trò
         const config = await logsCollection.findOne({ guildId: role.guild.id, eventType: 'roleCreate' });
         if (!config || !config.channelId) return;
 
         const logChannel = client.channels.cache.get(config.channelId);
         if (logChannel) {
             const embed = new EmbedBuilder()
-                .setTitle('🟢 Role Created')
+                .setTitle('🟢 Role đã được tạo')
                 .setColor('#00FF00')
                 .addFields(
                     { name: 'Role', value: `${role.name} (${role.id})`, inline: true },
-                    { name: 'Color', value: role.hexColor, inline: true },
+                    { name: 'Màu', value: role.hexColor, inline: true },
                 )
                 .setTimestamp();
 
