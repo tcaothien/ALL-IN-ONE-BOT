3a844a04066e1440071d3ab92b4d827924a2e872
tcaothien <116654835+tcaothien@users.noreply.github.com>
2025-02-01 21:19:42 +0700

    Update roleDeleteHandler.js

--- logHandlers/roleDeleteHandler.js
+++ logHandlers/roleDeleteHandler.js
@@ -1,14 +1,16 @@
 const { logsCollection } = require('../mongodb');
 const { EmbedBuilder } = require('discord.js');
+
 module.exports = async function roleDeleteHandler(client) {
     client.on('roleDelete', async (role) => {
+        // Lấy cấu hình ghi log cho sự kiện xóa vai trò
         const config = await logsCollection.findOne({ guildId: role.guild.id, eventType: 'roleDelete' });
         if (!config || !config.channelId) return;
 
         const logChannel = client.channels.cache.get(config.channelId);
         if (logChannel) {
             const embed = new EmbedBuilder()
-                .setTitle('🔴 Role Deleted')
+                .setTitle('🔴 Role đã bị xóa')
                 .setColor('#FF0000')
                 .addFields(
                     { name: 'Role', value: `${role.name} (${role.id})`, inline: true },
