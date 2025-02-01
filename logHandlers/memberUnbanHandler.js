f2e3f87c2acebc74ac531cd6d21960c12114b24c
tcaothien <116654835+tcaothien@users.noreply.github.com>
2025-02-01 21:12:58 +0700

    Update memberUnbanHandler.js

--- logHandlers/memberUnbanHandler.js
+++ logHandlers/memberUnbanHandler.js
@@ -1,21 +1,27 @@
+
 const { logsCollection } = require('../mongodb');
 const { EmbedBuilder } = require('discord.js');
+
 module.exports = async function memberUnbanHandler(client) {
     client.on('guildBanRemove', async (ban) => {
+        // Tìm kiếm cấu hình ghi log cho sự kiện gỡ cấm thành viên
         const config = await logsCollection.findOne({ guildId: ban.guild.id, eventType: 'memberUnban' });
         if (!config || !config.channelId) return;
 
+        // Lấy kênh ghi log từ ID đã cấu hình
         const logChannel = client.channels.cache.get(config.channelId);
         if (logChannel) {
+            // Tạo embed thông báo thành viên được gỡ cấm
             const embed = new EmbedBuilder()
-                .setTitle('🔓 Member Unbanned')
+                .setTitle('🔓 Thành viên được gỡ ban')
                 .setColor('#00FF00')
                 .addFields(
-                    { name: 'User', value: `${ban.user.tag} (${ban.user.id})`, inline: true },
+                    { name: 'Người dùng', value: `${ban.user.tag} (${ban.user.id})`, inline: true },
                 )
                 .setThumbnail(ban.user.displayAvatarURL())
                 .setTimestamp();
 
+            // Gửi thông báo vào kênh ghi log
             logChannel.send({ embeds: [embed] });
         }
     });
