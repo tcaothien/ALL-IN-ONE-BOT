1cd7ea8abd63a928f2a032623bb6361aaec9abac
tcaothien <116654835+tcaothien@users.noreply.github.com>
2025-02-01 09:37:10 +0700

    Update setlevellogs.js

--- commands/setups/setlevellogs.js
+++ commands/setups/setlevellogs.js
@@ -1,66 +1,64 @@
 const { SlashCommandBuilder, EmbedBuilder, PermissionsBitField } = require('discord.js');
 const { serverLevelingLogsCollection } = require('../../mongodb');
 const cmdIcons = require('../../UI/icons/commandicons');
+
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('setlevellogs')
-        .setDescription('Set the channel for leveling logs')
+        .setDescription('Cài đặt kênh cho nhật ký Level')
         .setDefaultMemberPermissions(PermissionsBitField.Flags.ManageGuild)
         .addStringOption(option =>
             option.setName('channelid')
-                .setDescription('The ID of the leveling logs channel')
+                .setDescription('ID của kênh nhật ký Level')
                 .setRequired(true)),
     async execute(interaction) {
         if (interaction.isCommand && interaction.isCommand()) {
-        if (!interaction.member.permissions.has(PermissionsBitField.Flags.ManageGuild)) {
+            if (!interaction.member.permissions.has(PermissionsBitField.Flags.ManageGuild)) {
+                const embed = new EmbedBuilder()
+                    .setColor('#ff0000')
+                    .setDescription('Bạn không có quyền sử dụng lệnh này.');
+                return interaction.reply({ embeds: [embed], ephemeral: true });
+            }
+
+            const channelId = interaction.options.getString('channelid');
+            const serverId = interaction.guild.id;
+
+            const channel = interaction.guild.channels.cache.get(channelId);
+            if (!channel) {
+                return interaction.reply({ content: 'ID kênh không hợp lệ. Vui lòng cung cấp ID kênh hợp lệ từ server này.', ephemeral: true });
+            }
+
+            try {
+                await serverLevelingLogsCollection.updateOne(
+                    { serverId },
+                    { $set: { levelLogsChannelId: channelId } },
+                    { upsert: true }
+                );
+
+                const embed = new EmbedBuilder()
+                    .setColor('#00ff00')
+                    .setDescription(`Nhật ký Level đã được cài đặt thành công vào kênh <#${channelId}>.`);
+                interaction.reply({ embeds: [embed] });
+            } catch (error) {
+                console.error('Lỗi khi cài đặt nhật ký Level:', error);
+
+                const errorEmbed = new EmbedBuilder()
+                    .setColor('#ff0000')
+                    .setDescription('Đã xảy ra lỗi khi cài đặt kênh nhật ký Level. Vui lòng thử lại sau.');
+                interaction.reply({ embeds: [errorEmbed], ephemeral: true });
+            }
+        } else {
             const embed = new EmbedBuilder()
-                .setColor('#ff0000')
-                .setDescription('You do not have permission to use this command.');
-            return interaction.reply({ embeds: [embed], ephemeral: true });
-        }
+                .setColor('#3498db')
+                .setAuthor({ 
+                    name: "Cảnh báo!", 
+                    iconURL: cmdIcons.dotIcon ,
+                    url: "https://discord.gg/enzlewy"
+                })
+                .setDescription('- Lệnh này chỉ có thể được sử dụng thông qua lệnh slash!\n- Vui lòng sử dụng `/setlevellogs`')
+                .setTimestamp();
 
-        const channelId = interaction.options.getString('channelid');
-        const serverId = interaction.guild.id;
-
-    
-        const channel = interaction.guild.channels.cache.get(channelId);
-        if (!channel) {
-            return interaction.reply({ content: 'Invalid channel ID. Please provide a valid channel ID from this server.', ephemeral: true });
-        }
-
-        try {
-          
-            await serverLevelingLogsCollection.updateOne(
-                { serverId },
-                { $set: { levelLogsChannelId: channelId } },
-                { upsert: true }
-            );
-
-            const embed = new EmbedBuilder()
-                .setColor('#00ff00')
-                .setDescription(`Leveling logs have been successfully set to <#${channelId}>.`);
-            interaction.reply({ embeds: [embed] });
-        } catch (error) {
-            console.error('Error setting leveling logs:', error);
-
-            const errorEmbed = new EmbedBuilder()
-                .setColor('#ff0000')
-                .setDescription('An error occurred while setting the leveling logs channel. Please try again later.');
-            interaction.reply({ embeds: [errorEmbed], ephemeral: true });
-        }
-    } else {
-        const embed = new EmbedBuilder()
-        .setColor('#3498db')
-        .setAuthor({ 
-            name: "Alert!", 
-            iconURL: cmdIcons.dotIcon ,
-            url: "https://discord.gg/xQF9f9yUEM"
-        })
-        .setDescription('- This command can only be used through slash command!\n- Please use `/setlevellogs`')
-        .setTimestamp();
-    
-        await interaction.reply({ embeds: [embed] });
-    
+            await interaction.reply({ embeds: [embed] });
         }  
     },
 };
